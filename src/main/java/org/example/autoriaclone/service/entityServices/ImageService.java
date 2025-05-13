2025ac76d053efcb305c9fff514aedcf9934d9ab
letniki <nikitaletuta12@gmai.com>
2025-05-13 14:14:25 +0300

    all done

--- src/main/java/org/example/autoriaclone/service/entityServices/ImageService.java
+++ src/main/java/org/example/autoriaclone/service/entityServices/ImageService.java
@@ -7,7 +7,6 @@ import org.example.autoriaclone.repository.CarRepository;
 import org.example.autoriaclone.repository.ImageRepository;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.util.List;
 
 @Data
@@ -17,13 +16,18 @@ public class ImageService {
     private final ImageRepository imageRepository;
 
     public String deleteImage(Integer imageId, Integer carId){
-        Car car = carRepository.findById(carId).get();
-        List<Image> images = car.getImages();
-        images.removeIf(image-> image.getId().equals(imageId));
-        car.setImages(images);
-        carRepository.save(car);
-        imageRepository.deleteById(imageId);
-        return "Image with id: "+imageId+" was deleted";
+        try{
+            Car car = carRepository.findById(carId).get();
+            List<Image> images = car.getImages();
+            images.removeIf(image-> image.getId().equals(imageId));
+            car.setImages(images);
+            carRepository.save(car);
+            imageRepository.deleteById(imageId);
+            return "Image with id: "+imageId+" was deleted";
+        } catch (Exception e){
+            return e.getMessage();
+        }
+
     }
 
 }
